2d407ae61edcb833dcc8be94516cfb5fe8f082d4
TolyaHuligan <d.zverkov14@gmail.com>
2024-12-19 14:08:53 +0700

    финал

--- main.py
+++ main.py
@@ -109,4 +109,39 @@ def gen_or_get_arr():
         print("Неверные данные")
 
 
-print(gen_or_get_arr())
+def menu(point):
+    if point == 1:
+        mas_a = gen_or_get_arr()
+        mas_b = gen_or_get_arr()
+        print("Сумма (1) или разность (2)")
+        x = int(input())
+        if x == 1:
+            print(add_large_numbers(mas_a, mas_b))
+        elif x == 2:
+            print(subtract_large_numbers(mas_a, mas_b))
+        else:
+            print("Error")
+    elif point == 2:
+        arr1 = gen_or_get_arr()
+        arr2 = gen_or_get_arr()
+        print(count_common_numbers(arr1,arr2))
+    elif point == 3:
+        arr = gen_or_get_arr()
+        print("Введите число: ")
+        num = int(input())
+        print(count_subarrays_with_sum(arr, num))
+    elif point == 4:
+        exit()
+    else:
+        print("Error")
+
+if __name__ == "__main__":
+    while True:
+        print("Задачи\n1.	Входные данные: 2 массива с цифрами, каждый представляет собой большое число. Нужно "
+              "произвести сумму или разность массивов\n2.	Входные данные: 2 массива с "
+              "числами. Требуется проверить, сколько у массивов общих чисел. Также, число будет считаться общим, "
+              "если оно входит в один массив, а в другом массиве находится его перевернутая версия.\n3.	Входные "
+              "данные: массив и число. Требуется проверить, "
+              "сколько подмассивов из массива в сумме могут давать это число\n4.  Выход")
+        print("Выберите пункт меню\n")
+        menu(int(input()))
